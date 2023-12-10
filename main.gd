4ad1e0007593364864a79515261e89f125851477
ChaoticPan <elisabeth.kintzel@student.htw-berlin.de>
2023-12-10 13:39:01 +0100

    algorithm works, but the rules i defined are shit

--- main.gd
+++ main.gd
@@ -1,17 +1,62 @@
 extends Node2D
-# KEY may appeare in any direction next to VALUE
-var rules = {
-			tileType.type.SEA:tileType.type.COAST,
-			tileType.type.COAST:tileType.type.SEA,
-			#tileType.type.COAST:tileType.type.LAND,
-			tileType.type.LAND:tileType.type.COAST
-			}
+var matrix=[]
+var rnd = RandomNumberGenerator.new()
+var finished = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	# why tf does godot not have 2-dimensional arrays i am going to scream
+	for x in range(10):
+		matrix.append([])
+		matrix[x]=[]	
+		for y in range(10):
+			matrix[x].append([])
+			matrix[x][y] = get_node(str(x)+str(y))
+			matrix[x][y].pos = Vector2(x,y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if finished:
+		return
+	var tile = getLowestEntrophy()
+	tile.collapse()
+	var x = tile.pos.x
+	var y = tile.pos.y
+	if x > 0:
+		propagation(tile, matrix[x-1][y])
+	if x < 9:
+		propagation(tile, matrix[x+1][y])
+	if y > 0:
+		propagation(tile, matrix[x][y-1])
+	if y < 9:
+		propagation(tile, matrix[x][y+1])
+
+func getLowestEntrophy():
+	var min = 4
+	var minTiles = []
+	
+	for x in range(10):
+		for y in range(10):
+			var a = matrix[x][y].getEntrophy()
+			if a==min:
+				minTiles.append(matrix[x][y])
+			elif a < min:
+				min = a
+				minTiles.clear()
+				minTiles.append(matrix[x][y])
+	if minTiles[0].getEntrophy() == 0:
+		finished = true
+	return minTiles[rnd.randf_range(0, minTiles.size())]
+
+func propagation(tile, neighbor):
+	var connections = tile.neighbors
+	var possible = neighbor.options
+	var newPoss = []
+	
+	#for each options in neigbor
+	for p in range(possible.size()):
+		var poss = possible[p]
+		#if option is in tile.neigbors
+		if  connections.find(poss) > -1:
+			newPoss.append(poss)
+	neighbor.options = newPoss
--- tile.gd
+++ tile.gd
@@ -1,27 +1,39 @@
 extends ColorRect
 
 @export var type = tileType.type.EMPTY 
-var options = {tileType.type.SEA: true, tileType.type.COAST: true, tileType.type.LAND: true}
+var options = [tileType.type.SEA, tileType.type.COAST, tileType.type.LAND]
+var neighbors = []
+var rnd = RandomNumberGenerator.new()
+var pos = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	setTile()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func setTile():
 	match type:
 		tileType.type.EMPTY:
-			color = Color("#FFFFFF")
+			color = Color("#9b9b9b")
+			neighbors = []
 		tileType.type.SEA:
 			color = Color("#00abff")
+			neighbors = [tileType.type.SEA, tileType.type.COAST]
 		tileType.type.COAST:
 			color = Color("#f3d472")
+			neighbors = [tileType.type.SEA, tileType.type.COAST, tileType.type.LAND]
 		tileType.type.LAND:
-			color = Color("#76da43")
+			color = Color("#63b834")
+			neighbors = [tileType.type.COAST, tileType.type.LAND]
 
-func countValidOptions():
-	var count = 0
-	for i in range(0, options.values().size()):
-		if options.values()[i] == true:
-			count+= 1
-	return count
+func getEntrophy():
+	if type != tileType.type.EMPTY:
+		return 4
+	return options.size()
+
+func collapse():
+	if type != tileType.type.EMPTY:
+		return 
+	var c = options[rnd.randf_range(0, options.size())]
+	type = c
+	options.clear()
+	setTile()
